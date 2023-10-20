0ebadf476f72ca2eef7a6d98091d342b2fed8533
Titus Anang <adoteititus1@gmail.com>
2023-10-19 22:46:59 -0700

    add script

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: negative, zero or positive
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	for (; *s1 == *s2; s1++, s2++)
+		if (*s1 == '\0')
+			return (0);
+	return (*s1 - *s2);
+}
