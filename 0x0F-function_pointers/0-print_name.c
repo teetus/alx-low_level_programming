2f228980835a74d593fb6ed6e8f4e13e707dd3a9
Tito <adoteititus1@gmail.com>
2023-11-08 21:49:44 -0800

    add script

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - prints a name.
+ * @name: string to print.
+ * @f: fuction to use.
+ *
+ * Return: NOTHING
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+		return;
+	f(name);
+}
